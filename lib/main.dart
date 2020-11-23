705840f7150479ef2002b3e335d94c266abc9d76
Luciana <arevalolucianadg@gmail.com>
2020-11-22 21:24:46 -0300

    Add MovieItem

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
+import 'src/pages/home_page.dart';
+import 'src/theme/ui_const.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build( context ) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      title: 'Movie App',
+    );
+    
+  }
+
+}
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
-import '../components/movie_list.dart';
-
+import '../components/shared/movie_item.dart';
 import '../theme/ui_const.dart';
 
 class HomePage extends StatelessWidget {
 
   final _title = new TextStyle(
     color: Colors.white,
-    fontSize: 30,
+    fontSize: 24,
     fontWeight: FontWeight.bold,
   );
   
@@ -21,20 +20,33 @@ class HomePage extends StatelessWidget {
         title: Text('FLUTT Movies'),
       ),
       backgroundColor: bgColor,
-      body: Padding(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget> [
-            Center(
-              child: Text('Popular Movies', style: _title),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Container(
+            child: Column(
+              children: <Widget> [
+                Center(
+                  child: Text('Popular Series', style: _title),
+                ),
+                Container(
+                  height: 700,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: ListView(
+                      children: <Widget>[
+                        MovieItem('/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg', 'The Flash', '7.6'),
+                        MovieItem('/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg', 'The Mandalorian', '8.5'),
+                        MovieItem('/7WTsnHkbA0FaG6R9twfFde0I9hl.jpg', 'Sherlock', '8.4'),
+                        SizedBox(height:40),
+                      ],
+                    )
+                  ),
+                  )
+              ]
             ),
-            Expanded(
-              child: MovieList(),
-            )
-          ]
+          ),
         ),
-        padding: EdgeInsets.all(16.0),
       ),
     );
     
